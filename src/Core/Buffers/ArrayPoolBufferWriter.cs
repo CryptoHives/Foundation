e4a838fd76db3c6de7c6eb8011d4422754a0b5d1
CryptoBee <cryptobee65@gmail.com>
2025-10-15 09:36:18 -0400

    add buffers

---
+++ src/Core/Buffers/ArrayPoolBufferWriter.cs
@@ -0,0 +1,171 @@
+// ------------------------------------------------------------
+//  Copyright (c) 2025 The Keepers of the CryptoHives.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace CryptoHives.Cryptography.Core.Buffers;
+
+using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Helper to build a <see cref="ReadOnlySequence{T}"/> from a set of buffers.
+/// Implements <see cref="IBufferWriter{T}"/> interface to support a writer
+/// for e.g. <see cref="System.Text.Json.Utf8JsonWriter"/>.
+/// </summary>
+public sealed class ArrayPoolBufferWriter<T> : IBufferWriter<T>, IDisposable
+{
+    private const int DefaultChunkSize = 256;
+    private const int MaxChunkSize = 65536;
+    private readonly bool _clearArray;
+    private readonly int _maxChunkSize;
+    private int _chunkSize;
+    private T[] _currentBuffer;
+    private ArrayPoolBufferSegment<T>? _firstSegment;
+    private ArrayPoolBufferSegment<T>? _nextSegment;
+    private int _offset;
+    private bool _disposed;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArrayPoolBufferWriter{T}"/> class.
+    /// </summary>
+    public ArrayPoolBufferWriter()
+        : this(false, DefaultChunkSize, MaxChunkSize)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArrayPoolBufferWriter{T}"/> class.
+    /// </summary>
+    public ArrayPoolBufferWriter(int defaultChunksize, int maxChunkSize)
+        : this(false, defaultChunksize, maxChunkSize)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArrayPoolBufferWriter{T}"/> class.
+    /// </summary>
+    public ArrayPoolBufferWriter(bool clearArray, int defaultChunksize, int maxChunkSize)
+    {
+        _firstSegment = _nextSegment = null;
+        _offset = 0;
+        _clearArray = clearArray;
+        _chunkSize = defaultChunksize;
+        _maxChunkSize = maxChunkSize;
+        _currentBuffer = Array.Empty<T>();
+        _disposed = false;
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (_firstSegment != null)
+        {
+            ArrayPoolBufferSegment<T>? segment = _firstSegment;
+            while (segment != null)
+            {
+                segment.Return(_clearArray);
+                segment = (ArrayPoolBufferSegment<T>?)segment.Next;
+            }
+
+            _firstSegment = _nextSegment = null;
+        }
+
+        _disposed = true;
+    }
+
+    /// <inheritdoc/>
+    public void Advance(int count)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(ArrayPoolBufferWriter<T>));
+        ArgumentOutOfRangeException.ThrowIfNegative(count, nameof(count));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(_offset + count, _currentBuffer.Length, $"Cannot advance past the end of the buffer, which has a size of {_currentBuffer.Length}.");
+
+        _offset += count;
+    }
+
+    /// <inheritdoc/>
+    public Memory<T> GetMemory(int sizeHint = 0)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(ArrayPoolBufferWriter<T>));
+        ArgumentOutOfRangeException.ThrowIfNegative(sizeHint, nameof(sizeHint));
+
+        int remainingSpace = CheckAndAllocateBuffer(sizeHint);
+        return _currentBuffer.AsMemory(_offset, remainingSpace);
+    }
+
+    /// <inheritdoc/>
+    public Span<T> GetSpan(int sizeHint = 0)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(ArrayPoolBufferWriter<T>));
+        ArgumentOutOfRangeException.ThrowIfNegative(sizeHint, nameof(sizeHint));
+
+        int remainingSpace = CheckAndAllocateBuffer(sizeHint);
+        return _currentBuffer.AsSpan(_offset, remainingSpace);
+    }
+
+    /// <summary>
+    /// Get a ReadOnlySequence that represents the written data.
+    /// The sequence is only valid until the next write operation or
+    /// until the writer is disposed.
+    /// </summary>
+    public ReadOnlySequence<T> GetReadOnlySequence()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(ArrayPoolBufferWriter<T>));
+
+        AddSegment();
+
+        if (_firstSegment == null || _nextSegment == null)
+        {
+            return ReadOnlySequence<T>.Empty;
+        }
+
+        return new ReadOnlySequence<T>(_firstSegment, 0, _nextSegment, _nextSegment.Memory.Length);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void AddSegment()
+    {
+        if (_offset > 0)
+        {
+            if (_firstSegment == null)
+            {
+                _firstSegment = _nextSegment = new ArrayPoolBufferSegment<T>(_currentBuffer, 0, _offset);
+            }
+            else
+            {
+                _nextSegment = _nextSegment!.Append(_currentBuffer, 0, _offset);
+            }
+        }
+        else if (_currentBuffer.Length > 0)
+        {
+            ArrayPool<T>.Shared.Return(_currentBuffer, _clearArray);
+        }
+
+        _offset = 0;
+        _currentBuffer = Array.Empty<T>();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private int CheckAndAllocateBuffer(int sizeHint)
+    {
+        int remainingSpace = _currentBuffer.Length - _offset;
+        if (remainingSpace < sizeHint || sizeHint == 0)
+        {
+            AddSegment();
+
+            remainingSpace = Math.Max(sizeHint, _chunkSize);
+
+            _currentBuffer = ArrayPool<T>.Shared.Rent(remainingSpace);
+            _offset = 0;
+
+            if (_chunkSize < _maxChunkSize)
+            {
+                _chunkSize *= 2;
+            }
+        }
+
+        return remainingSpace;
+    }
+}
---
+++ src/Core/Buffers/ReadOnlySequenceMemoryStream.cs
@@ -0,0 +1,234 @@
+// ------------------------------------------------------------
+//  Copyright (c) 2025 The Keepers of the CryptoHives.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace CryptoHives.Cryptography.Core.Buffers;
+
+using System;
+using System.Buffers;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Class to create a MemoryStream which uses a <see cref="ReadOnlySequence{T}"/>.
+/// </summary>
+public sealed class ReadOnlySequenceMemoryStream : MemoryStream
+{
+    private readonly ReadOnlySequence<byte> _sequence;
+    private SequencePosition _nextSequencePosition;
+    private long _sequenceOffset;
+    private ReadOnlyMemory<byte> _currentBuffer;
+    private int _currentOffset;
+    private bool _endOfStream;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ReadOnlySequenceMemoryStream"/> class.
+    /// </summary>
+    public ReadOnlySequenceMemoryStream(ReadOnlySequence<byte> sequence)
+    {
+        _sequence = sequence;
+        _nextSequencePosition = sequence.GetPosition(0);
+        _endOfStream = SetNextBuffer();
+    }
+
+    /// <inheritdoc/>
+    public override bool CanRead => true;
+
+    /// <inheritdoc/>
+    public override bool CanSeek => true;
+
+    /// <inheritdoc/>
+    public override bool CanWrite => false;
+
+    /// <inheritdoc/>
+    public override long Length => _sequence.Length;
+
+    /// <inheritdoc/>
+    public override long Position
+    {
+        get { return GetAbsolutePosition(); }
+        set { Seek(value, SeekOrigin.Begin); }
+    }
+
+    /// <inheritdoc/>
+    public override void Flush()
+    {
+        // nothing to do.
+    }
+
+    /// <inheritdoc/>
+    public override int ReadByte()
+    {
+        do
+        {
+            int bytesLeft = _currentBuffer.Length - _currentOffset;
+
+            // copy the bytes requested.
+            if (bytesLeft > 0)
+            {
+                return _currentBuffer.Span[_currentOffset++];
+            }
+
+            // move to next buffer.
+            if (!SetNextBuffer())
+            {
+                // end of stream.
+                return -1;
+            }
+        } while (true);
+    }
+
+    /// <inheritdoc/>
+    public override int Read(Span<byte> buffer)
+    {
+        int count = buffer.Length;
+        int offset = 0;
+        int bytesRead = 0;
+
+        while (count > 0)
+        {
+            int bytesLeft = _currentBuffer.Length - _currentOffset;
+            int bytesToCopy = Math.Min(bytesLeft, count);
+
+            // move to next buffer.
+            if (bytesToCopy <= 0)
+            {
+                if (!SetNextBuffer())
+                {
+                    return bytesRead;
+                }
+
+                continue;
+            }
+
+            // copy the bytes requested.
+            _currentBuffer.Span.Slice(_currentOffset, bytesToCopy).CopyTo(buffer.Slice(offset));
+            _currentOffset += bytesToCopy;
+            bytesRead += bytesToCopy;
+            offset += bytesToCopy;
+            count -= bytesToCopy;
+        }
+
+        return bytesRead;
+    }
+
+    /// <inheritdoc/>
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        int bytesRead = 0;
+
+        while (count > 0)
+        {
+            int bytesLeft = _currentBuffer.Length - _currentOffset;
+            int bytesToCopy = Math.Min(bytesLeft, count);
+
+            // move to next buffer.
+            if (bytesToCopy <= 0)
+            {
+                if (!SetNextBuffer())
+                {
+                    return bytesRead;
+                }
+
+                continue;
+            }
+
+            // copy the bytes requested.
+            _currentBuffer.Slice(_currentOffset, bytesToCopy).CopyTo(buffer.AsMemory(offset));
+            _currentOffset += bytesToCopy;
+            bytesRead += bytesToCopy;
+            offset += bytesToCopy;
+            count -= bytesToCopy;
+        }
+
+        return bytesRead;
+    }
+
+    /// <inheritdoc/>
+    public override long Seek(long offset, SeekOrigin loc)
+    {
+        switch (loc)
+        {
+            case SeekOrigin.Begin:
+                break;
+
+            case SeekOrigin.Current:
+                offset += GetAbsolutePosition();
+                break;
+
+            case SeekOrigin.End:
+                offset += _sequence.Length;
+                break;
+
+            default:
+                throw new IOException("Invalid seek origin value.");
+        }
+
+        if (offset < 0)
+        {
+            throw new IOException("Cannot seek beyond the beginning of the stream.");
+        }
+
+        // special case
+        if (offset > _sequence.Length)
+        {
+            throw new IOException("Cannot seek beyond the end of the stream.");
+        }
+
+        _nextSequencePosition = _sequence.GetPosition(offset);
+        if (!SetCurrentBuffer(offset))
+        {
+            throw new IOException("Cannot seek beyond the end of the stream.");
+        }
+
+        return GetAbsolutePosition();
+    }
+
+    /// <inheritdoc/>
+    public override void SetLength(long value) => throw new NotSupportedException();
+
+    /// <inheritdoc/>
+    public override byte[] ToArray() => _sequence.ToArray();
+
+    /// <summary>
+    /// Sets the current buffer.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool SetNextBuffer()
+    {
+        _sequenceOffset = _sequence.GetOffset(_nextSequencePosition);
+        _currentOffset = 0;
+        _endOfStream = _sequence.TryGet(ref _nextSequencePosition, out _currentBuffer, advance: true);
+        return _endOfStream;
+    }
+
+    /// <summary>
+    /// Sets the current buffer.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool SetCurrentBuffer(long offset)
+    {
+        _nextSequencePosition = _sequence.GetPosition(offset);
+        _sequenceOffset = _sequence.GetOffset(_nextSequencePosition);
+
+        _endOfStream = _sequence.TryGet(ref _nextSequencePosition, out _currentBuffer, advance: true);
+        long currentOffset = offset - _sequenceOffset;
+        if (currentOffset < 0 || (currentOffset >= _currentBuffer.Length && currentOffset > 0))
+        {
+            return false;
+        }
+
+        _currentOffset = (int)currentOffset;
+        return true;
+    }
+
+    /// <summary>
+    /// Returns the current position.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private long GetAbsolutePosition()
+    {
+        return _currentOffset + _sequenceOffset;
+    }
+}
