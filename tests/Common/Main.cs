2e301e81ed483809f2158773b370023eeb633402
Martin <mregen@live.de>
2025-11-01 10:21:22 +0100

    copyright tests

--- tests/Common/Main.cs
+++ tests/Common/Main.cs
@@ -1,7 +1,5 @@
-// ------------------------------------------------------------
-//  Copyright (c) 2025 The Keepers of the CryptoHives.  All rights reserved.
-//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// ------------------------------------------------------------
+// SPDX-FileCopyrightText: 2025 The Keepers of the CryptoHives
+// SPDX-License-Identifier: MIT
 
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
@@ -18,4 +16,3 @@ static class Program
         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
     }
 }
-
