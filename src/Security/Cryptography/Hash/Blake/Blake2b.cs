65cbccc25c7e2b822523b9b570a84a8d333ec755
The Keeper of the CryptoHives <cryptohivekeeper@gmail.com>
2026-01-13 15:00:46 +0100

    better blake2b

--- src/Security/Cryptography/Hash/Blake/Blake2b.cs
+++ src/Security/Cryptography/Hash/Blake/Blake2b.cs
@@ -12,6 +12,7 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 #if NET8_0_OR_GREATER
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 #endif
 
 /// <summary>
@@ -71,20 +72,20 @@ public sealed class Blake2b : HashAlgorithm
     ];
 
     // BLAKE2b sigma permutations for message scheduling
-    private static readonly byte[,] Sigma = new byte[Rounds, ScratchSize]
+    private static readonly byte[] Sigma = new byte[Rounds * ScratchSize]
     {
-        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
-        { 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 },
-        { 11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4 },
-        { 7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8 },
-        { 9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13 },
-        { 2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9 },
-        { 12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11 },
-        { 13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10 },
-        { 6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5 },
-        { 10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0 },
-        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
-        { 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 }
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3,
+        11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4,
+        7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8,
+        9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13,
+        2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9,
+        12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11,
+        13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10,
+        6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5,
+        10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3
     };
 
     private readonly ulong[] _state;
@@ -307,8 +308,8 @@ public sealed class Blake2b : HashAlgorithm
         _bytesCompressed += (ulong)_bufferLength;
 
         // Use stackalloc for working vectors to avoid heap allocations
-        Span<ulong> v = stackalloc ulong[16];
-        Span<ulong> m = stackalloc ulong[16];
+        Span<ulong> v = stackalloc ulong[ScratchSize];
+        Span<ulong> m = stackalloc ulong[ScratchSize];
 
         // Parse message block into 16 64-bit words (little-endian)
 #if NET8_0_OR_GREATER
@@ -319,18 +320,13 @@ public sealed class Blake2b : HashAlgorithm
             MemoryMarshal.Cast<byte, ulong>(block).CopyTo(m);
         }
         else
+#endif
         {
             for (int i = 0; i < ScratchSize; i++)
             {
                 m[i] = BinaryPrimitives.ReadUInt64LittleEndian(block.Slice(i * 8));
             }
         }
-#else
-        for (int i = 0; i < ScratchSize; i++)
-        {
-            m[i] = BinaryPrimitives.ReadUInt64LittleEndian(block.Slice(i * 8));
-        }
-#endif
 
         // Initialize working vector
         _state.CopyTo(v.Slice(0, StateSize));
@@ -340,19 +336,19 @@ public sealed class Blake2b : HashAlgorithm
         v[14] = isFinal ? ~IV[6] : IV[6];           // Invert if final block
 
         // 12 rounds of mixing
-        for (int round = 0; round < Rounds; round++)
+        for (int round = 0; round < Rounds * ScratchSize; round += ScratchSize)
         {
             // Column step
-            G(ref v[0], ref v[4], ref v[8], ref v[12], m[Sigma[round, 0]], m[Sigma[round, 1]]);
-            G(ref v[1], ref v[5], ref v[9], ref v[13], m[Sigma[round, 2]], m[Sigma[round, 3]]);
-            G(ref v[2], ref v[6], ref v[10], ref v[14], m[Sigma[round, 4]], m[Sigma[round, 5]]);
-            G(ref v[3], ref v[7], ref v[11], ref v[15], m[Sigma[round, 6]], m[Sigma[round, 7]]);
+            G(ref v[0], ref v[4], ref v[8], ref v[12], m[Sigma[round + 0]], m[Sigma[round + 1]]);
+            G(ref v[1], ref v[5], ref v[9], ref v[13], m[Sigma[round + 2]], m[Sigma[round + 3]]);
+            G(ref v[2], ref v[6], ref v[10], ref v[14], m[Sigma[round + 4]], m[Sigma[round + 5]]);
+            G(ref v[3], ref v[7], ref v[11], ref v[15], m[Sigma[round + 6]], m[Sigma[round + 7]]);
 
             // Diagonal step
-            G(ref v[0], ref v[5], ref v[10], ref v[15], m[Sigma[round, 8]], m[Sigma[round, 9]]);
-            G(ref v[1], ref v[6], ref v[11], ref v[12], m[Sigma[round, 10]], m[Sigma[round, 11]]);
-            G(ref v[2], ref v[7], ref v[8], ref v[13], m[Sigma[round, 12]], m[Sigma[round, 13]]);
-            G(ref v[3], ref v[4], ref v[9], ref v[14], m[Sigma[round, 14]], m[Sigma[round, 15]]);
+            G(ref v[0], ref v[5], ref v[10], ref v[15], m[Sigma[round + 8]], m[Sigma[round + 9]]);
+            G(ref v[1], ref v[6], ref v[11], ref v[12], m[Sigma[round + 10]], m[Sigma[round + 11]]);
+            G(ref v[2], ref v[7], ref v[8], ref v[13], m[Sigma[round + 12]], m[Sigma[round + 13]]);
+            G(ref v[3], ref v[4], ref v[9], ref v[14], m[Sigma[round + 14]], m[Sigma[round + 15]]);
         }
 
         // Finalize state
