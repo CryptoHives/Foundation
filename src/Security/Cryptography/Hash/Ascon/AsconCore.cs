1f1f4b8e0bf66bc4c09d3257ae6b5792ce766925
The Keeper of the CryptoHives <cryptohivekeeper@gmail.com>
2026-01-25 13:30:44 +0100

    clean up Ascon code

--- src/Security/Cryptography/Hash/Ascon/AsconCore.cs
+++ src/Security/Cryptography/Hash/Ascon/AsconCore.cs
@@ -18,12 +18,12 @@ using System.Runtime.CompilerServices;
 /// Reference implementation: <see href="https://github.com/ascon/ascon-c"/>
 /// </para>
 /// </remarks>
-internal static class Ascon800232Core
+internal static class AsconCore
 {
     /// <summary>
     /// Applies the Ascon permutation p^12.
     /// </summary>
-    [MethodImpl(MethodImplOptionsEx.OptimizedLoop)]
+    [MethodImpl(MethodImplOptionsEx.HotPath)]
     public static void P12(ref ulong s0, ref ulong s1, ref ulong s2, ref ulong s3, ref ulong s4)
     {
         Round(ref s0, ref s1, ref s2, ref s3, ref s4, 0xf0UL);
@@ -54,72 +54,5 @@ internal static class Ascon800232Core
         s2 = ~(t2 ^ BitOperations.RotateRight(t2, 1) ^ BitOperations.RotateRight(t2, 6));
         s3 = t3 ^ BitOperations.RotateRight(t3, 10) ^ BitOperations.RotateRight(t3, 17);
         s4 = t4 ^ BitOperations.RotateRight(t4, 7) ^ BitOperations.RotateRight(t4, 41);
-    }
-}
-
-/// <summary>
-/// Provides the Ascon permutation used by Ascon v1.2 (legacy).
-/// </summary>
-/// <remarks>
-/// <para>
-/// This is the original Ascon v1.2 specification permutation.
-/// For NIST SP 800-232, use <see cref="Ascon800232Core"/> instead.
-/// </para>
-/// </remarks>
-internal static class AsconCore
-{
-    /// <summary>
-    /// Applies the Ascon permutation p^r with the specified number of rounds.
-    /// </summary>
-    /// <param name="x0">State word 0.</param>
-    /// <param name="x1">State word 1.</param>
-    /// <param name="x2">State word 2.</param>
-    /// <param name="x3">State word 3.</param>
-    /// <param name="x4">State word 4.</param>
-    /// <param name="rounds">The number of rounds to apply (8 or 12).</param>
-    [MethodImpl(MethodImplOptionsEx.OptimizedLoop)]
-    public static void Permutation(ref ulong x0, ref ulong x1, ref ulong x2, ref ulong x3, ref ulong x4, int rounds)
-    {
-        unchecked
-        {
-            // Round constants: c_i = (0xf - i) << 4 | i for rounds 0-11
-            // For 12 rounds, start at round 0; for 8 rounds, start at round 4
-            int startRound = 12 - rounds;
-            for (int i = startRound; i < 12; i++)
-            {
-                // Addition of round constant to x2
-                // Round constants: 0xf0, 0xe1, 0xd2, 0xc3, 0xb4, 0xa5, 0x96, 0x87, 0x78, 0x69, 0x5a, 0x4b
-                x2 ^= (ulong)(((0xF - i) << 4) | i);
-
-                // Substitution layer (5-bit S-box applied to each bit slice)
-                x0 ^= x4;
-                x4 ^= x3;
-                x2 ^= x1;
-
-                ulong t0 = x0 & (~x1);
-                ulong t1 = x1 & (~x2);
-                ulong t2 = x2 & (~x3);
-                ulong t3 = x3 & (~x4);
-                ulong t4 = x4 & (~x0);
-
-                x0 ^= t1;
-                x1 ^= t2;
-                x2 ^= t3;
-                x3 ^= t4;
-                x4 ^= t0;
-
-                x1 ^= x0;
-                x0 ^= x4;
-                x3 ^= x2;
-                x2 = ~x2;
-
-                // Linear diffusion layer
-                x0 ^= BitOperations.RotateRight(x0, 19) ^ BitOperations.RotateRight(x0, 28);
-                x1 ^= BitOperations.RotateRight(x1, 61) ^ BitOperations.RotateRight(x1, 39);
-                x2 ^= BitOperations.RotateRight(x2, 1) ^ BitOperations.RotateRight(x2, 6);
-                x3 ^= BitOperations.RotateRight(x3, 10) ^ BitOperations.RotateRight(x3, 17);
-                x4 ^= BitOperations.RotateRight(x4, 7) ^ BitOperations.RotateRight(x4, 41);
-            }
-        }
     }
 }
